1e93c86e622b42fe5c188888681bf1ae965afb8b
SADmammoth <lo.ma200018@gmail.com>
2019-08-13 20:27:58 +0300

    fix: hyphenate work improve

--- hyphenate.js
+++ hyphenate.js
@@ -5,16 +5,13 @@ function hyphenate(text) {
     special = "[йъь'&-]",
     shy = "\xAD",
     regExps = [
-      new RegExp("(" + special + ")(" + anyletter + anyletter + ")", "ig"),
-      new RegExp("(" + vowel + ")(" + vowel + vowel + ")", "ig"),
-      new RegExp("(" + vowel + consonant + ")(" + consonant + vowel + ")", "ig"),
-      new RegExp("(" + vowel + vowel + ")(" + consonant + vowel + ")", "ig"),
-      new RegExp("(" + vowel + consonant + ")(" + consonant + consonant + vowel + ")", "ig"),
-      new RegExp("(" + vowel + consonant + consonant + ")(" + consonant + consonant + vowel + ")", "ig")
+      new RegExp(`(${vowel})(${anyletter}${anyletter})`, "ig"),
+      new RegExp(`(${vowel})(${consonant}${vowel}${anyletter})`, "ig"),
+      new RegExp(`(${vowel}${consonant})(${vowel}${anyletter})`, "ig")
     ];
 
   return function () {
-    for (var i = 1; i < 7; ++i) {
+    for (var i = 1; i < regExps.length; ++i) {
       text = text.replace(regExps[i], "$1" + shy + "$2");
     }
     return text
