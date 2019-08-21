6646dd5d7f326ee11f7f4af63815eb32be94f109
SADmammoth <lo.ma200018@gmail.com>
2019-08-21 22:51:19 +0300

    feat: add masters gallery page

--- hyphenate.js
+++ hyphenate.js
@@ -10,7 +10,7 @@ function hyphenate(text) {
       new RegExp(`(${anyletter}${vowel}${consonant})(${vowel}${anyletter})`, "ig")
     ];
 
-  return function () {
+  return function() {
     for (var i = 1; i < regExps.length; ++i) {
       text = text.replace(regExps[i], "$1" + shy + "$2");
     }
@@ -19,3 +19,8 @@ function hyphenate(text) {
 };
 
 document.body.querySelectorAll('.hyphenated').forEach(item => { console.log(item.innerText); return item.textContent = hyphenate(item.innerText)() });
+
+<
+script type = "text/javascript"
+charset = "utf-8"
+async src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A48aa09255f7b87136b085b14a724126378ab3965881b489652c6c51f68f6d150&amp;width=500&amp;height=500&amp;lang=ru_RU&amp;scroll=true" > < /script>
