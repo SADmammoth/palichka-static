d757f0613eb78f5b154889727c9f7530974541af
SADmammoth <lo.ma200018@gmail.com>
2019-08-22 16:15:54 +0300

    feat: contacts page

--- hyphenate.js
+++ hyphenate.js
@@ -7,7 +7,8 @@ function hyphenate(text) {
     regExps = [
       new RegExp(`(${anyletter}${vowel})(${anyletter}${anyletter})`, "ig"),
       new RegExp(`(${anyletter}${vowel})(${consonant}${vowel}${anyletter})`, "ig"),
-      new RegExp(`(${anyletter}${vowel}${consonant})(${vowel}${anyletter})`, "ig")
+      new RegExp(`(${anyletter}${vowel}${consonant})(${vowel}${anyletter})`, "ig"),
+      new RegExp(`(${vowel}${consonant})(${consonant}${vowel})`, "ig")
     ];
 
   return function() {
@@ -19,8 +20,3 @@ function hyphenate(text) {
 };
 
 document.body.querySelectorAll('.hyphenated').forEach(item => { console.log(item.innerText); return item.textContent = hyphenate(item.innerText)() });
-
-<
-script type = "text/javascript"
-charset = "utf-8"
-async src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A48aa09255f7b87136b085b14a724126378ab3965881b489652c6c51f68f6d150&amp;width=500&amp;height=500&amp;lang=ru_RU&amp;scroll=true" > < /script>
